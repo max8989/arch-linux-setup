58789d78a59a42624830620040130bc1d7b48e2b
Maxime Gagne <max.8989@hotmail.com>
2024-02-12 00:36:17 -0500

    install yay

--- instal_package.sh
+++ instal_package.sh
@@ -10,6 +10,26 @@ packages=(
 echo "Updating system package database..."
 sudo pacman -Sy
 
+# Install Yay
+if ! yay --version &> /dev/null; then
+    echo "yay is not installed, proceeding with installation..."
+    # Install base-devel and git if they are not already installed
+    sudo pacman -S --needed base-devel git
+
+    # Temporarily change to a temporary directory for yay installation
+    pushd "$(mktemp -d)" || exit 1
+
+    # Clone yay from AUR
+    git clone https://aur.archlinux.org/yay.git
+    cd yay || exit 1
+
+    # Build and install yay
+    makepkg -si
+
+    # Return to the original directory
+    popd
+fi
+
 # Loop through the list of packages and install if not already installed
 for package in "${packages[@]}"; do
     if ! pacman -Qi "$package" &> /dev/null; then
