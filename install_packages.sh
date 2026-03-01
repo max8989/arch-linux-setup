8f76a308448ceea892039da5928d6ee219e04b4e
Maxime Gagne <max.8989@hotmail.com>
2026-03-01 14:23:14 -0500

    add command to copy current dir

--- install_packages.sh
+++ install_packages.sh
@@ -320,6 +320,7 @@ add_alias_if_not_present 'alias ..="cd .."'
 add_alias_if_not_present 'alias ...="cd ../.."'
 add_alias_if_not_present 'alias neofetch="fastfetch"'
 add_alias_if_not_present 'alias pwdc="pwd | wl-copy"'
+add_alias_if_not_present 'alias cdc="pwd | xargs -I{} echo \"cd {}\" | wl-copy"'
 
 if [[ $setup_security == "y" ]]; then
   # Setup firewall rules
